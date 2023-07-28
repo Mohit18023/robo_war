832c5f7485b489b723dd9d36f91796ccb9ef7907
meenal6051 <116100273+meenal6051@users.noreply.github.com>
2023-07-28 18:31:31 +0530

    Update2 App.js

--- robo-war/src/App.js
+++ robo-war/src/App.js
@@ -5,22 +5,22 @@ function App() {
   return (
     <div className="App">
       <h1>this is changed</h1>
-      <p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <p>
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+    Edit <code>src/App.js</code> and save to reload.
+    </p>
+    <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </p>
-      </header>
+            Learn React
+            </a>
+            </header>
+            </p>
     </div>
   );
 }
