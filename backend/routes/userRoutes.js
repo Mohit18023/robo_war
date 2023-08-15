1088d7a9048ed768c785b24d4f8d6ec6eba18bb6
mohit choudhary <mohitchoudhary1054@gmail.com>
2023-08-15 21:26:53 +0530

    backend phase 1

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+//const { registerUser, authUser} = require('../controllers/userController.js');
+
+
+//router.route('/').post(registerUser);
+//router.post('/login', authUser);
+
+module.exports = router;
