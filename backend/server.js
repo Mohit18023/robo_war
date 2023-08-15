1572ac3bba98ae684e2727977af0f09d8b18ba8c
mohit choudhary <mohitchoudhary1054@gmail.com>
2023-08-15 21:26:53 +0530

    backend phase 1

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const connectDB = require('./config/db.js');
+const userRouter = require('./routes/userRoutes.js');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js');
+
+
+
+dotenv.config();
+
+connectDB();
+
+
+const app = express();
+app.use(express.json());
+
+app.use('/roboWar', userRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
