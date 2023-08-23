93cdb87883861424e4416185ba53cd071d8b231b
mohit choudhary <mohitchoudhary1054@gmail.com>
2023-08-23 20:28:03 +0530

    login signup api complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-//const { registerUser, authUser} = require('../controllers/userController.js');
+const { registerUser, authUser} = require('../controllers/userController.js');
 
 
-//router.route('/').post(registerUser);
-//router.post('/login', authUser);
+router.route('/').post(registerUser);
+router.post('/login', authUser);
 
 module.exports = router;
